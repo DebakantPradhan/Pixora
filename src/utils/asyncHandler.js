0f6c881ef8a0bf6d06e82a4c81a541fe63acbaa0
DebakantPradhan <debakantpradhan646@gmail.com>
2024-06-11 15:12:03 +0530

    add user and video model

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -35,3 +35,29 @@ export {asyncHandler}
 //         })
 //     }
 // }    //that err.code is a standard object err's property in express.js by default just like req,res etc
+
+
+/********************************************************************************************/
+
+
+
+// the above promise wala syntax works like this 
+
+/*
+
+// A callback function that logs the fetched data
+function logData(data) {
+console.log(data);
+}
+
+// A function that fetches data from an API and calls the callback function with the data
+function fetchData(url, callback) {
+fetch(url)
+.then(response => response.json())
+.then(data => callback(data)); // Call the callback function with the data
+}
+
+// Call the fetchData function with a URL and the logData function as arguments
+fetchData("https://example.com/api", logData);
+
+*/
