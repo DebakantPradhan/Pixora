828cddd179c23db0d3fb088ac521ae59d1398f9e
DebakantPradhan <debakantpradhan646@gmail.com>
2024-10-25 00:53:11 +0530

    feat: Add new models for comments, likes, playlists, and tweets

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -32,10 +32,12 @@ const userSchema = new Schema(
         coverImage:{
             type:String,        //Cloudinary url
         },
-        watchHistory:{
-            type:Schema.Types.ObjectId,         //Array of video ids
-            ref: "Video"   
-        },
+        watchHistory:[
+            {
+                type:Schema.Types.ObjectId,         //Array of video ids
+                ref: "Video"   
+            }
+        ],
         password:{
             type:String,
             required:[true,"Password is required."]
